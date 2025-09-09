7aa7186a3111118ca5064890bdc496fae3f93b5e
DESKTOP-58H1IT8\Vy Tran <dangvy202@gmail.com>
2025-09-09 18:07:06 +0700

    header

---
+++ header.php
@@ -0,0 +1,14 @@
+
+<!-- Navigation -->
+<div class="menu">
+<nav class="navbar" style="display:flex">
+    <div class="logo">WSUBook</div>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="signin.php">Sign In</a></li>
+        <li><a href="makebooking.php" class="active">Make Booking</a></li>
+        <li><a href="about.php">About</a></li>
+    </ul>
+</nav>
+</div>
